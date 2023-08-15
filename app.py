cb458ef6a6bbb8408d71a3e08663d672d038fcd0
pwxiao <3504852205@qq.com>
2023-08-15 09:13:56 +0800

    s

--- app.py
+++ app.py
@@ -12,14 +12,15 @@ def work():
     response.encoding = 'utf-8'
     html = response.text
     data = BeautifulSoup(html, 'html.parser')
-    matchNation = {'kr':"韩国",'cn':'中国','jp':'日本','tw':'中国台湾'}
+    genderColor = {'♂':"color:#0295FF",'♀':'color:#FE0097'}
+    
     csv_data = []
     for tr in data.find_all("tr"):
         tds = tr.find_all('td')
         if len(tds) == 5:
             rank = tds[0].text.strip()
             name = tds[1].text.strip()
-            # gender = tds[2].span.text.strip()
+            gender = tds[2].span.text.strip()
             nationality = str(tds[3].img['alt'])
             # try:
             #       nationality = matchNation[str(tds[3].img['alt'])]
@@ -27,6 +28,8 @@ def work():
             nationality = tds[3].img['alt']
             level = tds[4].text.strip()
             s = {}
+            s['gender'] = gender
+            s['genderColor'] = genderColor[gender]
             s['rank'] = rank
             s['name'] = name
             s['nationality'] = nationality
